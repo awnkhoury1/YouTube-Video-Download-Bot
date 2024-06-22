55f23b6fc325f28e43312e1f4b1c457382ca493e
awnkhoury1 <165429875+awnkhoury1@users.noreply.github.com>
2024-06-22 23:15:22 +0300

    script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -4,12 +4,11 @@ class Translation(object):
 
     ABOUT_TXT = """
 ╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
+├📛 **My Name** : [AK YouTube Mp4](https://t.me/AkYT4bot)
 ├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├👥 **Support Group** : [AK BOTS SUPPORT](https://t.me/AkBotsSupport)
+├🥏 **Channel** : [AK BOT CHANNEL](https://t.me/AkBots1st)
+├🎓 **Developer** : [Awn 👑](https://t.me/AwnXD)
 ╰───────────────⍟
 """
